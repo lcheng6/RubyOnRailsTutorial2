bb9a7448fba27bd3cef11de6e10bfe1df308a3bc
LiangCheng <liangcheng6@gmail.com>
2014-04-04 00:53:31 -0400

    Commit the first version of the Second Try Ruby on Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp2::Application.config.secret_key_base = '7c470acb8da1efb7df9156701d7dc7e9811b7882ec6ca677d4bbac8b1f741a45cc9aec7b0093aa61b301da5b9db26c74406a99abbc73116a25266e54ae960fee'

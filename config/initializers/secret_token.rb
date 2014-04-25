973049ba2c299533573df2e84969aec006d9df73
LiangCheng <liangcheng@example.com>
2014-04-25 12:22:58 -0700

    recheck secret_token.rb file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,7 +10,6 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 #SampleApp2::Application.config.secret_key_base = '7c470acb8da1efb7df9156701d7dc7e9811b7882ec6ca677d4bbac8b1f741a45cc9aec7b0093aa61b301da5b9db26c74406a99abbc73116a25266e54ae960fee'
-
 require 'securerandom'
 
 def secure_token

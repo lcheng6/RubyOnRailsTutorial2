797a1976cb4aa232277f75425da11ec97de13529
LiangCheng <liangcheng@example.com>
2014-04-27 14:46:20 -0700

    Finished layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp2::Application.routes.draw do
 
   root 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'

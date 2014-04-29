6bc65d5926c789634ec3b161d3af674c635a6e41
LiangCheng <liangcheng@example.com>
2014-04-29 18:58:50 -0400

    Adding uniqueness constraint to user table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
   has_many :microposts
   validates(:name, presence: true, length: {minimum:3, maximum: 50})
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
 end

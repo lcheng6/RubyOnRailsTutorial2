19aabb400b2085e5a7fa7bcc33a3c2c39e843ccc
LiangCheng <liangcheng@example.com>
2014-04-29 18:49:21 -0400

    Adding user specification and testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_many :microposts
+  validates(:name, presence: true, length: {minimum:3, maximum: 50})
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end

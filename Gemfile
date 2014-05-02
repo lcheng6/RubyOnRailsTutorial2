bf4951ea23d4519c2c97979d82199283730c6991
LiangCheng <liangcheng@example.com>
2014-05-01 22:24:15 -0400

    Done until the end of 7.1.3 of rails tutorial

--- Gemfile
+++ Gemfile
@@ -30,9 +30,9 @@ gem 'jquery-rails', '~>2.0.0'
 
 group :test do
   gem 'capybara', '~>2.2.0'
-  gem 'factory_girl_rails', '1.4.0'
   gem 'cucumber-rails', '1.2.1'
   gem 'database_cleaner', '0.7.0'
+  gem 'factory_girl_rails', '4.2.1'
 end
 
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ SampleApp2::Application.routes.draw do
 
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
-  match '/help', to: 'static_pages#help', via: 'get'
+  match '/help', to: 'static_pages#help',       via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
+  match '/about', to: 'static_pages#about',     via: 'get'
+
   resources :microposts
 
   resources :users

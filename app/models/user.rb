9d6614e1690db8d5b37ecd1a78202450664e5f35
LiangCheng <liangcheng@example.com>
2014-05-02 22:29:27 -0400

    Adding remember token to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-
   before_save { email.downcase! }
+  before_create :create_remember_token
 
   has_many :microposts
   validates(:name, presence: true, length: {minimum:3, maximum: 50})
@@ -10,4 +10,19 @@ class User < ActiveRecord::Base
 
   validates(:password, length: { minimum: 6} )
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      #Create the token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end

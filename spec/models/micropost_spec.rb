16dd89c018c0ef064be7146300e54353771b8e2b
LiangCheng <liangcheng@example.com>
2014-05-07 03:16:13 -0400

    Finished 10.1.3, before 10.1.4 Micropost refinements

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -12,4 +12,13 @@ describe Micropost do
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should eq user }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
 end

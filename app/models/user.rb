6815ff01c3ba9adf763977a73d52727a25a078ee
LiangCheng <liangcheng@example.com>
2014-05-09 19:37:11 -0400

    finished 11.1, about to start 11.2 A web interface for following users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
 
+  has_many :reverse_relationships, foreign_key: "followed_id",
+           class_name: "Relationship",
+           dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   validates(:name, presence: true, length: {minimum:3, maximum: 50})
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },

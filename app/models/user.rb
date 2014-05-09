164335817f096b0fb82c24c8a3c6799534ed1b56
LiangCheng <liangcheng@example.com>
2014-05-09 03:07:34 -0400

    Finished until listing 11.10

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   validates(:name, presence: true, length: {minimum:3, maximum: 50})
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
